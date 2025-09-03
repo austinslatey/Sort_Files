966d44f60965524ec77327e3aa55f33f4cab7669
Austin Slater <aslater@waldoch.com>
2025-09-03 12:51:44 -0500

    updated to use file generated from terminal.js for a more dynamic approach

--- outputToCSV.js
+++ outputToCSV.js
@@ -1,25 +1,13 @@
 const fs = require('fs');
 
-// Your file list as a string
-const fileList = `
-28-31015_p03_6.jpg
-28-31015_p05_2.jpg
-28-31035_ko3qtrdr_p04.jpg
-28-31035_kodir_p01_1.jpg
-28-31035_p05_2.jpg
-28-31045_kodir_p04.jpg
-28-31045_p03_1.jpg
-28-31095_ko3qtrps_p01_2.jpg
-28-31095_p05_2.jpg
-28-31135_ko3qtrdr_p04.jpg
-28-31135_ko3qtrps_p01_2.jpg
-28-31275_ko3qtrdr_p04.jpg
-28-31275_kodir_p01_1.jpg
-28-32165_p05_1.jpg
-28-32425_p05_2.jpg
-28-32785_ko3qtrdr_p04.jpg
-28-32785_kodir_p01_1.jpg
-`;
+// Read the file list from files.txt
+let fileList;
+try {
+  fileList = fs.readFileSync('./files.txt', 'utf8');
+} catch (err) {
+  console.error('Error reading files.txt:', err);
+  process.exit(1); // Exit if the file can't be read
+}
 
 // Split the list into lines and filter out empty entries
 const files = fileList.trim().split('\n').filter(file => file);
